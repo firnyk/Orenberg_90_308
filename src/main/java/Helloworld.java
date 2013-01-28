09417fab24399b0b58f51d1e8f2c6427cb01189d
Matt Orenberg <firnyk@gmail.com>
2013-01-28 13:38:39 -0500

    initial commit

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ *
+ * @author Orenberg_M
+ */
+public class Helloworld {
+    public static void main (String args[]){
+        System.out.println("Hello world!");
+    }
+}
