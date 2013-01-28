5985d6a7f8b05957de8af2229e3f0588cad96002
Matt Orenberg <firnyk@gmail.com>
2013-01-28 14:07:17 -0500

    changed text

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -6,6 +6,6 @@ package main.java;
  */
 public class Helloworld {
     public static void main (String args[]){
-        System.out.println("Hello world!");
+        System.out.println("Hello Git!");
     }
 }
